dde295aaf2d128c34b8b7cca652b6c6609c83c34
ralfiyka <zaydullin.rasid@mail.ru>
2022-08-07 12:41:24 +0300

    Sm chngs

--- main.py
+++ main.py
@@ -4,37 +4,45 @@ import os.path
 import re
 import string
 import requests
+import telebot
+import config
+import urllib.parse
+from telebot import types
 from googleapiclient.discovery import build
 from google_auth_oauthlib.flow import InstalledAppFlow
 from google.auth.transport.requests import Request
 from google.oauth2.credentials import Credentials
 
+bot = telebot.TeleBot(config.TOKEN)
+
 SCOPES = ['https://www.googleapis.com/auth/calendar']
 
+group = ''
 
-def main():
+
+def main(group):
     days, corr, corrtime, ans, creds = ('Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница',
                                         'Суббота'), [], [], [], None
-    thissite = (requests.get(url='https://eners.kgeu.ru/apish2.php?group=%D0%90%D0%A2-1-21&week=17&type=one').text)
+    thissite = (requests.get(url='https://eners.kgeu.ru/apish2.php?group=' + group + '&week=17&type=one').text)
     """Shows basic usage of the Google Calendar API.
-    Prints the start and name of the next 10 events on the user's calendar.
-    """
-    creds = None
-    if os.path.exists('torenhere'):
-        creds = Credentials.from_authorized_user_file('sametokenhere', SCOPES)
-    if not creds or not creds.valid:
-        if creds and creds.expired and creds.refresh_token:
-            creds.refresh(Request())
-        else:
-            flow = InstalledAppFlow.from_client_secrets_file(
-                'clientsecretoftokenhere', SCOPES)
-            creds = flow.run_local_server(port=0)
-        with open('sametokenhere', 'w') as token:
-            token.write(creds.to_json())
+    # Prints the start and name of the next 10 events on the user's calendar.
+    # """
+    # creds = None
+    # if os.path.exists('torenhere'):
+    #     creds = Credentials.from_authorized_user_file('sametokenhere', SCOPES)
+    # if not creds or not creds.valid:
+    #     if creds and creds.expired and creds.refresh_token:
+    #         creds.refresh(Request())
+    #     else:
+    #         flow = InstalledAppFlow.from_client_secrets_file(
+    #             'clientsecretoftokenhere', SCOPES)
+    #         creds = flow.run_local_server(port=0)
+    #     with open('sametokenhere', 'w') as token:
+    #         token.write(creds.to_json())
+    #
+    # service = build('calendar', 'v3', credentials=creds)
 
-    service = build('calendar', 'v3', credentials=creds)
-
-# Editin string of site
+    # Editin string of site
     for el in thissite.split():
         let = str(list(string.ascii_letters))
         new_string = el.translate(str.maketrans('', '', string.punctuation))
@@ -45,9 +53,13 @@ def main():
         for el in days:
             if len(corr[i]) == 4 and corr[i - 6] == el:
                 if len(corr[i - 5]) != 8:
-                    ans.append('0' + corr[i - 5][0] + '-' + corr[i - 5][1:3] + '-' + corr[i - 5][3:7] + ' ' + corr[i][:2] + ':' + corr[i][2:])
+                    ans.append('0' + corr[i - 5][0] + '-' + corr[i - 5][1:3] + '-' + corr[i - 5][3:7] + ' ' + corr[i][
+                                                                                                              :2] + ':' +
+                               corr[i][2:])
                 else:
-                    ans.append(corr[i - 5][:2] + '-' + corr[i - 5][2:4] + '-' + corr[i - 5][4:8] + ' ' + corr[i][:2] + ':' + corr[i][2:])
+                    ans.append(
+                        corr[i - 5][:2] + '-' + corr[i - 5][2:4] + '-' + corr[i - 5][4:8] + ' ' + corr[i][:2] + ':' +
+                        corr[i][2:])
     for i in range(len(ans)):
         cmall = datetime.datetime.strptime(ans[i], "%d-%m-%Y %H:%M")
         cm = str(cmall)
@@ -60,8 +72,20 @@ def main():
         }
         # event = (service.events().insert(calendarId='primary', body=event).execute())
         # print('Event created: %s' % (event.get('htmlLink')))
-        print(thissite)
+    return ans
 
 
 if __name__ == '__main__':
-    main()
+    main(input())
+
+# @bot.message_handler(content_types=['text'])
+# def get_messages(message):
+#     if message.text == '/start':
+#         bot.send_message(message.from_user.id,
+#                          "Привет! Я бот⏱. В нем ты сможешь автоматически ставить будильники для начала пар.В ответ пришли мне номер группы.")
+#     else:
+#         group = message.text
+#         bot.send_message(message.from_user.id, main(urllib.parse.quote(group)))
+#
+#
+# bot.polling(none_stop=True)
